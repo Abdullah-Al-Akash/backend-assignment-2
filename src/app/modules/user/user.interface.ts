b0aeb610eea2587bded85748e9b80cd9c8003f43
Abdullah Al Akash <aabdullahalakash@gmail.com>
2023-11-24 13:54:19 +0600

    Added User Interface

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,32 @@
+/* eslint-disable no-unused-vars */
+import { Model } from 'mongoose';
+
+export type TOrder = {
+  productName: string;
+  price: number;
+  quantity: number;
+};
+
+export type TUser = {
+  userId: number;
+  username: string;
+  password: string;
+  fullName: {
+    firstName: string;
+    lastName: string;
+  };
+  age: number;
+  email: string;
+  isActive: boolean;
+  hobbies: string[];
+  address: {
+    street: string;
+    city: string;
+    country: string;
+  };
+  orders?: TOrder[];
+};
+
+export interface IUserModel extends Model<TUser> {
+  isUserExists(userId: number): Promise<TUser | null>;
+}
